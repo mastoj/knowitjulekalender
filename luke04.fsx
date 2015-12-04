1ac7d2b40fe4db807e2a4e9e08b79634a42ae2b0
Tomas Jansson <tomas.jansson@gmail.com>
2015-12-04 09:53:31 +0100

    Update luke04.fsx

--- luke04.fsx
+++ luke04.fsx
@@ -34,4 +34,4 @@ let result =
     |> Seq.sortBy (fun (k,_) -> k)
     |> Seq.map (fun (k, v) -> sprintf "%c%i" k (v |> Seq.length))
     |> Seq.toList
-    |> (fun x -> String.Join(", ", x))
+    |> String.concat
