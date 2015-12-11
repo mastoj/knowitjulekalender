99c2a2204bb92457701c81341d679ee9972e7416
Tomas Jansson <tomas.jansson@gmail.com>
2015-12-11 14:30:33 +0100

    finished klingon

--- klingon.fsx
+++ klingon.fsx
@@ -1,3 +1,4 @@
+open System
 let numbers = 
     [
         0, "pagh"
@@ -12,6 +13,9 @@ let numbers =
         9, "Hut"
     ] |> Map.ofList
 
+let klingonNumbersToDec = 
+    numbers |> Map.toSeq |> Seq.map (fun (k,v) -> v,k) |> Map.ofSeq
+
 let tens =
     [
         10, "maH"
@@ -21,6 +25,9 @@ let tens =
         100000, "bIp"
         1000000, "’uy’"
     ] |> Map.ofList
+
+let klingonTensToDec = 
+    tens |> Map.toSeq |> Seq.map (fun (k,v) -> v,k) |> Map.ofSeq
 
 let decToKlingon dec = 
     if dec = 0 then numbers |> Map.find 0
@@ -41,11 +48,30 @@ let decToKlingon dec =
                     decToKlingon' y (div/10) (sprintf "%s %s%s" acc num tenth)
         decToKlingon' dec (1000000) "" |> (fun s -> s.Trim())
 
-let res = 
+let klingonToDec klingon =
+    let parse (word:string) =
+        let keyOption = klingonTensToDec |> Map.tryFindKey (fun k v -> word.EndsWith(k))
+        match keyOption with
+        | None -> 
+            klingonNumbersToDec |> Map.find word
+        | Some k -> 
+            let ten = klingonTensToDec |> Map.find k
+            let num = klingonNumbersToDec |> Map.find (word.Replace(k, ""))
+            ten * num
+
+    let words = klingon |> (fun (s:string) -> s.Split([|' '|])) |> List.ofArray
+    words
+    |> List.map parse
+    |> List.sum
+
+let nums = 
     [
         9999999
         0
         4234134
         23
         23424    
-    ] |> List.map decToKlingon        
+    ] 
+let klingonNums = nums |> List.map decToKlingon
+let shouldBeTrue = nums = (klingonNums |> List.map klingonToDec)        
+
