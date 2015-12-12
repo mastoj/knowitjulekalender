09e29f20e3ec2ced1e5ee6fe9288faebaf65453d
Tomas Jansson <tomas.jansson@gmail.com>
2015-12-12 20:46:50 +0100

    added one faster luke12

--- luke12.fsx
+++ luke12.fsx
@@ -1,1 +1,14 @@
+#time
 seq {for x in 7L .. 7L .. 100000000L do if x%5L<>0L then yield x} |> Seq.sum
+#time
+
+#time
+let rec doLuke12 current max acc =
+    match current with
+    | x when x > max -> acc
+    | x -> 
+        if x%5L <> 0L then doLuke12 (current+7L) max (acc+x)
+        else doLuke12 (current+7L) max acc
+
+let res = doLuke12 7L 100000000L 0L
+#time   
