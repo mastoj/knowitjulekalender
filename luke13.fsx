47fab462dd98de0507d3b4018d1aca2da89ba067
Tomas Jansson <tomas.jansson@gmail.com>
2015-12-13 23:56:12 +0100

    luke 13

--- luke13.fsx
+++ luke13.fsx
@@ -1,44 +1,18 @@
-#load "Helpers.fsx"
-
-open Math
+open System.Collections.Generic
+open System.Numerics
 
 let n = 10000
-// let sieve = createPrimeSieve (n*100)
-// let nonKnallSieve = sieve |> List.filter (fun i -> i<>2 && i<>3 && i<>5) |> List.rev
-//     
-// let isKnallTall n =
-//     if n = 1 then true
-//     else
-//         nonKnallSieve |> List.exists (fun x -> n%x=0) |> not
-// 
-// Seq.initInfinite (fun i -> i+1)
-// |> Seq.filter isKnallTall
-// |> Seq.skip (n-1)
-// |> Seq.head
-// |> printfn "%A"
-
-
-
-let knallTallSeq() = 
-    let rec knallTallSeq' candidate knallTall =
-        seq {
-            let div = knallTall |> List.tryFind (fun i -> candidate%i=0)
-            match div with
-            | Some x ->
-                if knallTall |> List.exists (fun i -> candidate/x=i)
-                then 
-                    yield candidate
-                    yield! knallTallSeq' (candidate+1) (candidate::knallTall)
-                else yield! knallTallSeq' (candidate+1) knallTall
-            | None -> yield! knallTallSeq' (candidate+1) knallTall
-        }
-    seq {
-        yield 1
-        yield 2
-        yield 3
-        yield 4
-        yield 5
-        yield! knallTallSeq' 6 [2;3;4;5]
-    }
+let knallTallList() =
+    [ for x in 0 .. 60 do
+        for y in 0 .. 60 do
+            for z in 0 .. 60 do
+            yield 
+                BigInteger.Pow((bigint 2), x) *
+                BigInteger.Pow((bigint 3), y) *
+                BigInteger.Pow((bigint 5), z)
+    ] |> List.sort
     
-let res = knallTallSeq() |> Seq.skip (n-1) |> Seq.take 10 |> Seq.toList |> printfn "%A"
+#time
+let res = knallTallList() |> List.skip 9999 |> List.head |> printfn "The winner is: %A"
+#time
+
