e27e24b94dc6b628c232d0cc8899a6f26b0236c0
Tomas Jansson <tomas.jansson@unacast.com>
2018-12-01 22:20:02 +0100

    luke 2018-01

---
+++ 2018/luke01.fsx
@@ -0,0 +1,24 @@
+#load "../helpers/WebHelper.fsx"
+open WebHelper
+
+let fileUrl = "https://s3-eu-west-1.amazonaws.com/knowit-julekalender-2018/input-vekksort.txt"
+
+let data = download fileUrl
+let numbers = data |> Seq.map int64 |> List.ofSeq
+
+let solve numbers =
+    numbers 
+    |> Seq.fold 
+                (
+                    fun (prev, acc) n -> 
+                    if n < prev
+                    then (prev, acc)
+                    else (n, acc + n)
+                ) (0L, 0L)
+    |> snd
+
+#time
+let result = numbers |> solve
+
+printfn "%A" result
+#time
